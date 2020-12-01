3c604a6171ccc1f0331b02a29c547bf71c970954
Steve Astels <sastels@gmail.com>
2020-12-01 12:13:29 -0500

    day 1a

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,16 @@
-pub fn pr() {
-    println!("Hello, day 1!");
+use std::collections::HashMap;
+
+pub fn solve_a(data: Vec<String>) {
+    let data = data.iter().map(|s| s.parse::<i32>().unwrap());
+    let mut numbers_seen = HashMap::new();
+    for num in data {
+        match numbers_seen.get(&(2020 - num)) {
+            Some(_) => {
+                println!("Day 1a: {}, {} -> {}", num, 2020 - num, num * (2020 - num));
+            }
+            None => {
+                numbers_seen.insert(num, 1);
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use advent_2020::day1;
+use advent_2020::utils::read_lines;
 
 fn main() {
-    day1::pr();
+    let data = read_lines("./data/day1.txt");
+    day1::solve_a(data);
 }
