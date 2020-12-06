73efe75fe8311c9f852dbc9bce3f988497ad0dfb
Steve Astels <sastels@gmail.com>
2020-12-06 11:19:12 -0500

    a

--- src/day6.rs
+++ src/day6.rs
@@ -1,9 +1,19 @@
-// use std::collections::HashSet;
+use crate::utils::join_lines;
+use itertools::Itertools;
 
-pub fn solve_a(_data: &[String]) -> usize {
-    0
+pub fn solve_a(data: &[String]) -> usize {
+    let groups = join_lines(data);
+    groups
+        .iter()
+        .map(|line| {
+            line.chars()
+                .filter(|c| *c != ' ') // join_lines substitutes spaces for newlines
+                .unique()
+                .count()
+        })
+        .sum()
 }
 
-pub fn solve_b(_data: &[String]) -> usize {
+pub fn solve_b(data: &[String]) -> usize {
     0
 }
--- tests/day6.rs
+++ tests/day6.rs
@@ -3,13 +3,13 @@ use advent_2020::day6;
 use advent_2020::utils::read_lines;
 
 #[test]
-fn test_day5_solve_a() {
-    let data = read_lines("./data/day6.txt");
-    assert_eq!(day6::solve_a(&data), 666);
+fn test_day6_solve_a() {
+    let data = read_lines("./data/day6_test.txt");
+    assert_eq!(day6::solve_a(&data), 11);
 }
 
 #[test]
-fn test_day5_solve_b() {
+fn test_day6_solve_b() {
     let data = read_lines("./data/day6.txt");
-    assert_eq!(day6::solve_b(&data), 666);
+    assert_eq!(day6::solve_b(&data), 6);
 }
