a10e1b26c7bc2567a116c8e1347d2e8eb55a4ccf
Steve Astels <sastels@gmail.com>
2020-12-07 11:33:15 -0500

    rule

--- src/day7.rs
+++ src/day7.rs
@@ -1,7 +1,36 @@
 // use crate::utils::join_lines;
 // use itertools::Itertools;
+use regex::Regex;
+use std::collections::HashMap;
 
-pub fn solve_a(_data: &[String]) -> usize {
+#[derive(Debug)]
+pub struct Rule {
+    pub outside: String,
+    pub inside: HashMap<String, usize>,
+}
+
+impl Rule {
+    pub fn new(line: &str) -> Self {
+        let line = line.replace("bags", "bag").replace(".", "");
+        let re = Regex::new(r"(.+) contain (.+)").unwrap();
+        let cap = re.captures(&line).unwrap();
+        let outside = cap[1].to_string();
+        let mut inside: HashMap<String, usize> = HashMap::new();
+        for bag in cap[2].split(", ") {
+            if bag != "no other bag" {
+                let re = Regex::new(r"(\d+) (.+)").unwrap();
+                let bag_match = re.captures(bag).unwrap();
+                inside.insert(
+                    bag_match[2].to_string(),
+                    bag_match[1].parse::<usize>().unwrap(),
+                );
+            }
+        }
+        Rule { outside, inside }
+    }
+}
+pub fn solve_a(data: &[String]) -> usize {
+    let rules: Vec<Rule> = data.iter().map(|line| Rule::new(line)).collect();
     0
 }
 
