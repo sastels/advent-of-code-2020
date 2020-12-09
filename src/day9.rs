13454774bcc715c565ee0f71e2428a1b4df73eb7
Steve Astels <sastels@gmail.com>
2020-12-09 12:32:07 -0500

    a

--- src/day9.rs
+++ src/day9.rs
@@ -1,4 +1,24 @@
-pub fn solve_a(_data: &[String], _period: usize) -> i32 {
+use std::collections::HashSet;
+
+pub fn all_sums(data: &[usize]) -> HashSet<usize> {
+    let mut sums: HashSet<usize> = HashSet::new();
+    for x in data {
+        for y in data {
+            if x != y {
+                sums.insert(x + y);
+            }
+        }
+    }
+    sums
+}
+
+pub fn solve_a(data: &[String], period: usize) -> usize {
+    let data: Vec<usize> = data.iter().map(|x| x.parse::<usize>().unwrap()).collect();
+    for i in period..data.len() {
+        if !all_sums(&data[i - period..i]).contains(&data[i]) {
+            return data[i];
+        }
+    }
     0
 }
 
--- tests/day9.rs
+++ tests/day9.rs
@@ -1,11 +1,20 @@
 #[cfg(test)]
-use advent_2020::day9;
+use advent_2020::day9::{self, all_sums};
 use advent_2020::utils::read_lines;
+use std::collections::HashSet;
+
+#[test]
+
+fn test_day9_all_sums() {
+    let actual = all_sums(&[1, 2, 3]);
+    let expected: HashSet<usize> = vec![3, 4, 5].into_iter().collect();
+    assert_eq!(actual, expected);
+}
 
 #[test]
 fn test_day9_solve_a() {
     let data = read_lines("./data/day9_test.txt");
-    assert_eq!(day9::solve_a(&data, 5), 179);
+    assert_eq!(day9::solve_a(&data, 5), 127);
 }
 
 #[test]
