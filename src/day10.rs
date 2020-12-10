fb0d6303d28454fa133ffd29111dc46e496a7c5d
Steve Astels <sastels@gmail.com>
2020-12-10 11:38:22 -0500

    a

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,21 @@
-pub fn solve_a(_data: &[String]) -> usize {
-    unimplemented!();
+use std::collections::HashMap;
+
+pub fn solve_a(data: &[String]) -> usize {
+    let mut data: Vec<usize> = data.iter().map(|x| x.parse::<usize>().unwrap()).collect();
+    data.push(0);
+    data.push(data.iter().max().unwrap() + 3);
+    data.sort();
+
+    let mut diffs: HashMap<usize, usize> = HashMap::new();
+    for i in 1..data.len() {
+        let counter = diffs.entry(data[i] - data[i - 1]).or_insert(0);
+        *counter += 1;
+    }
+    diffs[&1] * diffs[&3]
+}
+
+pub fn is_chain_valid(_data: &[usize]) -> bool {
+    true
 }
 
 pub fn solve_b(_data: &[String]) -> usize {
--- tests/day10.rs
+++ tests/day10.rs
@@ -1,11 +1,24 @@
 #[cfg(test)]
-use advent_2020::day10;
+use advent_2020::day10::{self, is_chain_valid};
 use advent_2020::utils::read_lines;
 
 #[test]
 fn test_day10_solve_a() {
     let data = read_lines("./data/day10_test_1.txt");
-    assert_eq!(day10::solve_a(&data), 0);
+    assert_eq!(day10::solve_a(&data), 7 * 5);
+}
+
+#[test]
+fn test_day10_is_chain_valid_true() {
+    assert!(is_chain_valid([0, 1, 2]));
+    assert!(is_chain_valid([2, 5, 8]));
+    assert!(is_chain_valid([0, 1, 3, 5, 6, 9]));
+}
+
+#[test]
+fn test_day10_is_chain_valid_false() {
+    assert!(!is_chain_valid([0, 4]));
+    assert!(!is_chain_valid([0, 2, 4, 20]));
 }
 
 #[test]
