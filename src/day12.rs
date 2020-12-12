dbdb2a6afe6a30798908cbe91e078527be96c2fc
Steve Astels <sastels@gmail.com>
2020-12-12 13:19:18 -0500

    b

--- src/day12.rs
+++ src/day12.rs
@@ -3,16 +3,23 @@ use std::fmt;
 pub struct Ship {
     pub name: String,
     pub direction: i32,
-    pub latitude: i32,
-    pub longitude: i32,
+    pub waypoint_lat: i32,
+    pub waypoint_long: i32,
+    pub position_lat: i32,
+    pub position_long: i32,
 }
 
 impl fmt::Display for Ship {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "{} : lat {}  long {} direction: {}",
-            self.name, self.latitude, self.longitude, self.direction
+            "{} : waypoint: ({}, {}) position ({}, {}) direction: {}",
+            self.name,
+            self.waypoint_lat,
+            self.waypoint_long,
+            self.position_lat,
+            self.position_long,
+            self.direction
         )
     }
 }
@@ -22,29 +29,77 @@ impl Ship {
         Ship {
             name: name.to_string(),
             direction: 90,
-            latitude: 0,
-            longitude: 0,
+            position_lat: 0,
+            position_long: 0,
+            waypoint_lat: 10,
+            waypoint_long: 1,
         }
     }
 
     pub fn execute_command(&mut self, command: &str) {
-        let action = command.chars().nth(0).unwrap();
+        let action = command.chars().next().unwrap();
         let value: i32 = command[1..].parse().unwrap();
-
         match action {
-            'N' => self.longitude += value,
-            'S' => self.longitude -= value,
-            'E' => self.latitude += value,
-            'W' => self.latitude -= value,
+            'N' => self.position_long += value,
+            'S' => self.position_long -= value,
+            'E' => self.position_lat += value,
+            'W' => self.position_lat -= value,
             'R' => self.direction += value,
             'L' => self.direction -= value,
             'F' => match self.direction {
-                0 => self.longitude += value,
-                90 => self.latitude += value,
-                180 => self.longitude -= value,
-                270 => self.latitude -= value,
+                0 => self.position_long += value,
+                90 => self.position_lat += value,
+                180 => self.position_long -= value,
+                270 => self.position_lat -= value,
                 _ => panic!(),
             },
+            _ => panic!(),
+        }
+        self.direction = ((self.direction % 360) + 360) % 360;
+    }
+
+    pub fn rotate_waypoint_r_90(&mut self) {
+        let new_lat = self.waypoint_long;
+        let new_long = -self.waypoint_lat;
+        self.waypoint_lat = new_lat;
+        self.waypoint_long = new_long;
+    }
+
+    pub fn rotate_waypoint_l_90(&mut self) {
+        let new_lat = -self.waypoint_long;
+        let new_long = self.waypoint_lat;
+        self.waypoint_lat = new_lat;
+        self.waypoint_long = new_long;
+    }
+
+    pub fn execute_command_b(&mut self, command: &str) {
+        let action = command.chars().next().unwrap();
+        let value: i32 = command[1..].parse().unwrap();
+        match action {
+            'N' => self.waypoint_long += value,
+            'S' => self.waypoint_long -= value,
+            'E' => self.waypoint_lat += value,
+            'W' => self.waypoint_lat -= value,
+            'R' => {
+                for _ in 0..value / 90 {
+                    let new_lat = self.waypoint_long;
+                    let new_long = -self.waypoint_lat;
+                    self.waypoint_lat = new_lat;
+                    self.waypoint_long = new_long;
+                }
+            }
+            'L' => {
+                for _ in 0..value / 90 {
+                    let new_lat = -self.waypoint_long;
+                    let new_long = self.waypoint_lat;
+                    self.waypoint_lat = new_lat;
+                    self.waypoint_long = new_long;
+                }
+            }
+            'F' => {
+                self.position_lat += self.waypoint_lat * value;
+                self.position_long += self.waypoint_long * value;
+            }
             _ => panic!(),
         }
         self.direction = ((self.direction % 360) + 360) % 360;
@@ -56,9 +111,13 @@ pub fn solve_a(data: &[String]) -> i32 {
     for command in data {
         ship.execute_command(command);
     }
-    ship.latitude.abs() + ship.longitude.abs()
+    ship.position_lat.abs() + ship.position_long.abs()
 }
 
-pub fn solve_b(_data: &[String]) -> usize {
-    0
+pub fn solve_b(data: &[String]) -> i32 {
+    let mut ship = Ship::new("Bluenose");
+    for command in data {
+        ship.execute_command_b(command);
+    }
+    ship.position_lat.abs() + ship.position_long.abs()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,61 @@
-// use advent_2020::day1;
-// use advent_2020::day10;
-// use advent_2020::day11;
+use advent_2020::day1;
+use advent_2020::day10;
+use advent_2020::day11;
 use advent_2020::day12;
-// use advent_2020::day2;
-// use advent_2020::day3;
-// use advent_2020::day4;
-// use advent_2020::day5;
-// use advent_2020::day6;
-// use advent_2020::day7;
-// use advent_2020::day8;
-// use advent_2020::day9;
+use advent_2020::day2;
+use advent_2020::day3;
+use advent_2020::day4;
+use advent_2020::day5;
+use advent_2020::day6;
+use advent_2020::day7;
+use advent_2020::day8;
+use advent_2020::day9;
 use advent_2020::utils::read_lines;
 
 fn main() {
-    // let data = read_lines("./data/day1.txt");
-    // println!("Day 1 A: {}", day1::solve_a(&data));
-    // println!("Day 1 B: {}", day1::solve_b(&data));
+    let data = read_lines("./data/day1.txt");
+    println!("Day 1 A: {}", day1::solve_a(&data));
+    println!("Day 1 B: {}", day1::solve_b(&data));
 
-    // let data = read_lines("./data/day2.txt");
-    // println!("Day 2 A: {}", day2::solve_a(&data));
-    // println!("Day 2 B: {}", day2::solve_b(&data));
+    let data = read_lines("./data/day2.txt");
+    println!("Day 2 A: {}", day2::solve_a(&data));
+    println!("Day 2 B: {}", day2::solve_b(&data));
 
-    // let data = read_lines("./data/day3.txt");
-    // println!("Day 3 A: {}", day3::solve_a(&data));
-    // println!("Day 3 B: {}", day3::solve_b(&data));
+    let data = read_lines("./data/day3.txt");
+    println!("Day 3 A: {}", day3::solve_a(&data));
+    println!("Day 3 B: {}", day3::solve_b(&data));
 
-    // let data = read_lines("./data/day4.txt");
-    // println!("Day 4 A: {}", day4::solve_a(&data));
-    // println!("Day 4 B: {}", day4::solve_b(&data));
+    let data = read_lines("./data/day4.txt");
+    println!("Day 4 A: {}", day4::solve_a(&data));
+    println!("Day 4 B: {}", day4::solve_b(&data));
 
-    // let data = read_lines("./data/day5.txt");
-    // println!("Day 5 A: {}", day5::solve_a(&data));
-    // println!("Day 5 B: {}", day5::solve_b(&data));
+    let data = read_lines("./data/day5.txt");
+    println!("Day 5 A: {}", day5::solve_a(&data));
+    println!("Day 5 B: {}", day5::solve_b(&data));
 
-    // let data = read_lines("./data/day6.txt");
-    // println!("Day 6 A: {}", day6::solve_a(&data));
-    // println!("Day 6 B: {}", day6::solve_b(&data));
+    let data = read_lines("./data/day6.txt");
+    println!("Day 6 A: {}", day6::solve_a(&data));
+    println!("Day 6 B: {}", day6::solve_b(&data));
 
-    // let data = read_lines("./data/day7.txt");
-    // println!("Day 7 A: {}", day7::solve_a(&data));
-    // println!("Day 7 B: {}", day7::solve_b(&data));
+    let data = read_lines("./data/day7.txt");
+    println!("Day 7 A: {}", day7::solve_a(&data));
+    println!("Day 7 B: {}", day7::solve_b(&data));
 
-    // let data = read_lines("./data/day8.txt");
-    // println!("Day 8 A: {}", day8::solve_a(&data));
-    // println!("Day 8 B: {}", day8::solve_b(&data));
+    let data = read_lines("./data/day8.txt");
+    println!("Day 8 A: {}", day8::solve_a(&data));
+    println!("Day 8 B: {}", day8::solve_b(&data));
 
-    // let data = read_lines("./data/day9.txt");
-    // println!("Day 9 A: {}", day9::solve_a(&data, 25));
-    // println!("Day 9 B: {}", day9::solve_b(&data, 466456641));
+    let data = read_lines("./data/day9.txt");
+    println!("Day 9 A: {}", day9::solve_a(&data, 25));
+    println!("Day 9 B: {}", day9::solve_b(&data, 466456641));
 
-    // let data = read_lines("./data/day10.txt");
-    // println!("Day 10 A: {}", day10::solve_a(&data));
-    // println!("Day 10 B: {}", day10::solve_b(&data));
+    let data = read_lines("./data/day10.txt");
+    println!("Day 10 A: {}", day10::solve_a(&data));
+    println!("Day 10 B: {}", day10::solve_b(&data));
 
-    // let data = read_lines("./data/day11.txt");
-    // println!("Day 11 A: {}", day11::solve_a(&data));
-    // println!("Day 11 B: {}", day11::solve_b(&data));
+    let data = read_lines("./data/day11.txt");
+    println!("Day 11 A: {}", day11::solve_a(&data));
+    println!("Day 11 B: {}", day11::solve_b(&data));
 
     let data = read_lines("./data/day12.txt");
     println!("Day 12 A: {}", day12::solve_a(&data));
