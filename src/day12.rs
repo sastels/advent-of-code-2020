9b74bd86dafe9166548fce32b51e7ec48a4727ba
Steve Astels <sastels@gmail.com>
2020-12-12 12:57:39 -0500

    a

--- src/day12.rs
+++ src/day12.rs
@@ -1,7 +1,64 @@
-pub fn solve_a(_data: &[String]) -> usize {
-    unimplemented!();
+use std::fmt;
+
+pub struct Ship {
+    pub name: String,
+    pub direction: i32,
+    pub latitude: i32,
+    pub longitude: i32,
+}
+
+impl fmt::Display for Ship {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{} : lat {}  long {} direction: {}",
+            self.name, self.latitude, self.longitude, self.direction
+        )
+    }
+}
+
+impl Ship {
+    pub fn new(name: &str) -> Self {
+        Ship {
+            name: name.to_string(),
+            direction: 90,
+            latitude: 0,
+            longitude: 0,
+        }
+    }
+
+    pub fn execute_command(&mut self, command: &str) {
+        let action = command.chars().nth(0).unwrap();
+        let value: i32 = command[1..].parse().unwrap();
+
+        match action {
+            'N' => self.longitude += value,
+            'S' => self.longitude -= value,
+            'E' => self.latitude += value,
+            'W' => self.latitude -= value,
+            'R' => self.direction += value,
+            'L' => self.direction -= value,
+            'F' => match self.direction {
+                0 => self.longitude += value,
+                90 => self.latitude += value,
+                180 => self.longitude -= value,
+                270 => self.latitude -= value,
+                _ => panic!(),
+            },
+            _ => panic!(),
+        }
+        self.direction = ((self.direction % 360) + 360) % 360;
+    }
+}
+
+pub fn solve_a(data: &[String]) -> i32 {
+    let mut ship = Ship::new("Bluenose");
+    for command in data {
+        ship.execute_command(command);
+    }
+    ship.latitude.abs() + ship.longitude.abs()
 }
 
 pub fn solve_b(_data: &[String]) -> usize {
-    unimplemented!();
+    0
 }
--- tests/day12.rs
+++ tests/day12.rs
@@ -5,11 +5,11 @@ use advent_2020::utils::read_lines;
 #[test]
 fn test_day12_solve_a() {
     let data = read_lines("./data/day12_test.txt");
-    assert_eq!(day12::solve_a(&data), 0);
+    assert_eq!(day12::solve_a(&data), 25);
 }
 
 #[test]
 fn test_day12_solve_b() {
     let data = read_lines("./data/day12_test.txt");
-    assert_eq!(day12::solve_b(&data), 0);
+    assert_eq!(day12::solve_b(&data), 286);
 }
