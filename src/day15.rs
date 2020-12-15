f88e8dd159533b5ac0f63cde933cf98ef40e3b57
Steve Astels <sastels@users.noreply.github.com>
2020-12-15 10:42:46 -0500

    a and b (#15)

--- src/day15.rs
+++ src/day15.rs
@@ -1,7 +1,56 @@
-pub fn solve_a(_data: &[String]) -> usize {
-    unimplemented!()
+use std::collections::HashMap;
+
+pub fn solve_a(data: &[String]) -> usize {
+    let input: Vec<usize> = data[0]
+        .split(",")
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
+
+    let mut last_said: HashMap<usize, usize> = HashMap::new();
+    let mut new_number: usize = 0;
+    for i in 0..2020 {
+        let last_number = new_number;
+        if i < input.len() {
+            new_number = input[i];
+        } else {
+            if last_said.get(&last_number).is_none() {
+                new_number = 0;
+            } else {
+                new_number = i - 1 - last_said.get(&last_number).unwrap()
+            }
+        }
+        if i > 0 {
+            last_said.insert(last_number, i - 1);
+        }
+    }
+    new_number
 }
 
-pub fn solve_b(_data: &[String]) -> usize {
-    unimplemented!()
+pub fn solve_b(data: &[String]) -> usize {
+    let input: Vec<usize> = data[0]
+        .split(",")
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
+
+    let mut last_said: HashMap<usize, usize> = HashMap::new();
+    let mut new_number: usize = 0;
+    for i in 0..30000000 {
+        if i % 100000 == 0 {
+            println!("{} / {}", i / 100000, 300);
+        }
+        let last_number = new_number;
+        if i < input.len() {
+            new_number = input[i];
+        } else {
+            if last_said.get(&last_number).is_none() {
+                new_number = 0;
+            } else {
+                new_number = i - 1 - last_said.get(&last_number).unwrap()
+            }
+        }
+        if i > 0 {
+            last_said.insert(last_number, i - 1);
+        }
+    }
+    new_number
 }
--- tests/day15.rs
+++ tests/day15.rs
@@ -2,15 +2,14 @@ use advent_2020::day15::{solve_a, solve_b};
 use advent_2020::utils::read_lines;
 
 #[test]
-#[ignore]
 fn a() {
-    let data = read_lines("./data/day13_test.txt");
-    assert_eq!(solve_a(&data), 0);
+    let data = read_lines("./data/day15_test.txt");
+    assert_eq!(solve_a(&data), 436);
 }
 
 #[test]
 #[ignore]
 fn b() {
-    let data = read_lines("./data/day13_test.txt");
+    let data = read_lines("./data/day15_test.txt");
     assert_eq!(solve_b(&data), 0);
 }
