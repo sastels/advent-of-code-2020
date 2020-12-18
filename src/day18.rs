ecdba006f8d8ba76ce9f645fb97c79b65ba23811
Steve Astels <sastels@users.noreply.github.com>
2020-12-18 11:42:40 -0500

    Day18 (#18)

--- src/day18.rs
+++ src/day18.rs
@@ -1,7 +1,60 @@
-pub fn solve_a(_data: &[String]) -> usize {
-    unimplemented!()
+use regex::Regex;
+
+pub fn calc(line: &str) -> usize {
+    let mut eqn = line.to_string();
+
+    // calculate parentheses
+    let re = Regex::new(r"(.*)\(([\d\w \+\*]+)\)(.*)").unwrap();
+    loop {
+        let cap = re.captures(&eqn);
+        match cap {
+            None => break,
+            Some(cap) => {
+                let val = calc(&cap[2]);
+                eqn = format!("{}{}{}", cap[1].to_string(), val, cap[3].to_string());
+            }
+        }
+    }
+
+    // calculate adds - just for part b
+    let re = Regex::new(r"^(.*?)(\d+) \+ (\d+)(.*?)$").unwrap();
+    loop {
+        let cap = re.captures(&eqn);
+        match cap {
+            None => break,
+            Some(cap) => {
+                let left_side = cap[1].to_string();
+                let left_dig = cap[2].parse::<usize>().unwrap();
+                let right_dig = cap[3].parse::<usize>().unwrap();
+                let right_side = cap[4].to_string();
+                eqn = format!("{}{}{}", left_side, left_dig + right_dig, right_side);
+            }
+        }
+    }
+
+    // now there's just multiplies
+    let mut value = 0;
+    let mut operation = "+";
+
+    for element in eqn.split(" ") {
+        match element.parse::<usize>() {
+            Err(_) => operation = element,
+            Ok(num) => {
+                if operation == "+" {
+                    value += num;
+                } else {
+                    value *= num;
+                }
+            }
+        }
+    }
+    value
 }
 
-pub fn solve_b(_data: &[String]) -> usize {
-    unimplemented!()
+pub fn solve_a(data: &[String]) -> usize {
+    data.iter().map(|s| calc(s)).sum()
+}
+
+pub fn solve_b(data: &[String]) -> usize {
+    data.iter().map(|s| calc(s)).sum()
 }
--- tests/day18.rs
+++ tests/day18.rs
@@ -1,5 +1,20 @@
-use advent_2020::day18::{solve_a, solve_b};
+use advent_2020::day18::{calc, solve_a, solve_b};
 use advent_2020::utils::read_lines;
+
+#[test]
+fn test_calc_b() {
+    // assert_eq!(calc("2 + 3"), 5);
+    // assert_eq!(calc("4 * 5"), 20);
+    // assert_eq!(calc("2 * 3 + 20"), 46);
+
+    // assert_eq!(calc("2 * 3 + (4 * 5)"), 46);
+    assert_eq!(calc("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 1445);
+    assert_eq!(calc("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), 669060);
+    assert_eq!(
+        calc("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"),
+        23340
+    );
+}
 
 #[test]
 #[ignore]
