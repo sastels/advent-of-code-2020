9ed23f514ad28a9b104c8a7584ed4158c03a6d68
Steve Astels <sastels@gmail.com>
2020-12-19 15:50:57 -0500

    parse rules

--- src/day19.rs
+++ src/day19.rs
@@ -1,3 +1,37 @@
+pub struct Rule {
+    pub id: String,
+    pub lhs: Vec<String>,
+    pub rhs: Vec<String>,
+}
+
+impl Rule {
+    pub fn new(line: &str) -> Self {
+        let line = line.replace("\"", "");
+        let mut split = line.split(": ");
+        let id = split.next().unwrap().to_string();
+        let rest = split.next().unwrap();
+        let mut split = rest.split(" | ");
+        let lhs = split
+            .next()
+            .unwrap()
+            .to_string()
+            .split(" ")
+            .map(|s| s.to_string())
+            .collect();
+        let mut rhs: Vec<String> = vec![];
+        let rhs_str = split.next();
+        if rhs_str.is_some() {
+            rhs = rhs_str
+                .unwrap()
+                .to_string()
+                .split(" ")
+                .map(|s| s.to_string())
+                .collect();
+        }
+        Rule { id, lhs, rhs }
+    }
+}
+
 pub fn solve_a(_data: &[String]) -> usize {
     unimplemented!()
 }
--- tests/day19.rs
+++ tests/day19.rs
@@ -1,5 +1,28 @@
-use advent_2020::day19::{solve_a, solve_b};
+use advent_2020::day19::{solve_a, solve_b, Rule};
 use advent_2020::utils::read_lines;
+
+#[test]
+fn rule_new_1() {
+    let rule = Rule::new("0: 4 1 5");
+    assert_eq!(rule.id, "0");
+    assert_eq!(rule.lhs, vec!["4", "1", "5"]);
+    assert_eq!(rule.rhs, Vec::<String>::new());
+}
+
+#[test]
+fn rule_new_2() {
+    let rule = Rule::new("4: \"a\"");
+    assert_eq!(rule.lhs, vec!["a"]);
+    assert_eq!(rule.rhs, Vec::<String>::new());
+}
+
+#[test]
+fn rule_new_3() {
+    let rule = Rule::new("12: 11 2 | 312 56");
+    assert_eq!(rule.id, "12");
+    assert_eq!(rule.lhs, vec!["11", "2"]);
+    assert_eq!(rule.rhs, vec!["312", "56"]);
+}
 
 #[test]
 #[ignore]
