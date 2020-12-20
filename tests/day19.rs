49144329a9365342e6b105d7ccbbe949a7a0755e
Steve Astels <sastels@gmail.com>
2020-12-19 20:13:14 -0500

    lots of refactoring

--- tests/day19.rs
+++ tests/day19.rs
@@ -1,27 +1,35 @@
-use advent_2020::day19::{matches, parse_input, solve_a, solve_b, Rule};
+use advent_2020::day19::{find_first_diff_pos, matches, parse_input, solve_a, solve_b, Rule};
 use advent_2020::utils::read_lines;
+
+#[test]
+fn diff_strings() {
+    assert_eq!(find_first_diff_pos("abc", "abc"), None);
+    assert_eq!(find_first_diff_pos("abd", "abc"), Some(2));
+    assert_eq!(find_first_diff_pos("ab", "abc"), Some(2));
+    assert_eq!(find_first_diff_pos("ab", ""), Some(0));
+}
 
 #[test]
 fn rule_new_1() {
     let rule = Rule::new("0: 4 1 5");
     assert_eq!(rule.id, "0");
-    assert_eq!(rule.lhs, vec!["4", "1", "5"]);
-    assert_eq!(rule.rhs, Vec::<String>::new());
+    assert_eq!(rule.lhs, "4 1 5");
+    assert_eq!(rule.rhs, "");
 }
 
 #[test]
 fn rule_new_2() {
     let rule = Rule::new("4: \"a\"");
-    assert_eq!(rule.lhs, vec!["a"]);
-    assert_eq!(rule.rhs, Vec::<String>::new());
+    assert_eq!(rule.lhs, "a");
+    assert_eq!(rule.rhs, "");
 }
 
 #[test]
 fn rule_new_3() {
     let rule = Rule::new("12: 11 2 | 312 56");
     assert_eq!(rule.id, "12");
-    assert_eq!(rule.lhs, vec!["11", "2"]);
-    assert_eq!(rule.rhs, vec!["312", "56"]);
+    assert_eq!(rule.lhs, "11 2");
+    assert_eq!(rule.rhs, "312 56");
 }
 
 #[test]
@@ -29,7 +37,7 @@ fn parse_input_rules() {
     let data = read_lines("./data/day19_test.txt");
     let (rules, _) = parse_input(&data);
     assert_eq!(rules.len(), 6);
-    assert_eq!(rules.get("2").unwrap().lhs, vec!["4", "4"])
+    assert_eq!(rules.get("2").unwrap().lhs, "4 4")
 }
 
 #[test]
@@ -41,20 +49,44 @@ fn parse_input_messages() {
 }
 
 #[test]
-
-fn matches_1() {
+fn matches_simple() {
     let data = read_lines("./data/day19_test.txt");
     let (rules, _) = parse_input(&data);
-    assert!(matches("ababbb", "0", &rules));
-    assert!(matches("aba", "aba", &rules));
-    assert!(!matches("bababa", "0", &rules));
+    assert!(matches("aba", "a b a", &rules));
+    assert!(matches("abbbab", "a b b b a b", &rules));
+    assert!(!matches("ba", "a b a", &rules));
+    assert!(!matches("adbbbab", "a b b b a b", &rules));
 }
 
 #[test]
-#[ignore]
+fn matches_medium() {
+    let data = read_lines("./data/day19_test.txt");
+    let (rules, _) = parse_input(&data);
+    assert!(matches("a", "4", &rules));
+    assert!(matches("b", "5", &rules));
+    assert!(matches("aab", "4 4 5", &rules));
+    assert!(!matches("aa", "4", &rules));
+    assert!(!matches("a", "5", &rules));
+    assert!(!matches("aabb", "4 4 5", &rules));
+}
+
+#[test]
+fn matches_complicated() {
+    let data = read_lines("./data/day19_test.txt");
+    let (rules, _) = parse_input(&data);
+    assert!(matches("ab", "3", &rules));
+    assert!(matches("ba", "3", &rules));
+    assert!(!matches("baa", "3", &rules));
+    assert!(matches("ababbb", "0", &rules));
+    assert!(matches("abbbab", "0", &rules));
+    assert!(!matches("bababa", "0", &rules));
+    assert!(!matches("aaaabbb", "0", &rules));
+}
+
+#[test]
 fn a() {
     let data = read_lines("./data/day19_test.txt");
-    assert_eq!(solve_a(&data), 0);
+    assert_eq!(solve_a(&data), 2);
 }
 
 #[test]
