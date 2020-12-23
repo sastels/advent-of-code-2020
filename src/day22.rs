fe2af6079d96f726bbac7d34b3c9a2d0e474ea8b
Steve Astels <sastels@gmail.com>
2020-12-23 15:08:58 -0500

    b

--- src/day22.rs
+++ src/day22.rs
@@ -1,5 +1,4 @@
 use crate::utils::join_lines;
-use std::cmp;
 
 pub fn deal(decks: &mut Vec<String>) -> (Vec<usize>, Vec<usize>) {
     // bottom of deck is first value. so to get top you pop()
@@ -38,21 +37,20 @@ pub fn calc_score(deck: &[usize]) -> usize {
 }
 
 pub fn wins_hand(deck: &mut Vec<usize>, card1: usize, card2: usize) {
-    deck.insert(0, cmp::max(card1, card2));
-    deck.insert(0, cmp::min(card1, card2));
+    deck.insert(0, card1);
+    deck.insert(0, card2);
 }
 
 pub fn solve_a(data: &[String]) -> usize {
     let mut data = join_lines(data);
     let (mut deck1, mut deck2) = deal(&mut data);
 
-    // play the game!
     loop {
         if let Some((card1, card2)) = draw_cards(&mut deck1, &mut deck2) {
             if card1 > card2 {
                 wins_hand(&mut deck1, card1, card2);
             } else {
-                wins_hand(&mut deck2, card1, card2);
+                wins_hand(&mut deck2, card2, card1);
             }
         } else {
             break;
@@ -66,27 +64,58 @@ pub fn solve_a(data: &[String]) -> usize {
     }
 }
 
+// return the winning player and change the decks (changing the decks needed for final score)
+pub fn recursive_combat(round: usize, deck1: &mut Vec<usize>, deck2: &mut Vec<usize>) -> usize {
+    let mut previous_rounds: Vec<String> = vec![];
+
+    loop {
+        let round_description = format!("P1 {:?} P2 {:?}", deck1, deck2);
+        if previous_rounds.contains(&round_description) {
+            return 1;
+        } else {
+            previous_rounds.push(round_description);
+            if let Some((card1, card2)) = draw_cards(deck1, deck2) {
+                if deck1.len() >= card1 && deck2.len() >= card2 {
+                    let mut rec_deck1: Vec<usize> =
+                        deck1.iter().rev().take(card1).rev().map(|n| *n).collect();
+                    let mut rec_deck2: Vec<usize> =
+                        deck2.iter().rev().take(card2).rev().map(|n| *n).collect();
+                    let winner = recursive_combat(round + 1, &mut rec_deck1, &mut rec_deck2);
+                    if winner == 1 {
+                        wins_hand(deck1, card1, card2);
+                    } else {
+                        wins_hand(deck2, card2, card1);
+                    }
+                } else {
+                    if card1 > card2 {
+                        wins_hand(deck1, card1, card2);
+                    } else {
+                        wins_hand(deck2, card2, card1);
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+    }
+    if deck1.is_empty() {
+        2
+    } else {
+        1
+    }
+}
+
 pub fn solve_b(data: &[String]) -> usize {
     let mut data = join_lines(data);
     let (mut deck1, mut deck2) = deal(&mut data);
-    let mut previous_rounds: Vec<String> = vec![];
 
-    // play the game!
-    loop {
-        if let Some((card1, card2)) = draw_cards(&mut deck1, &mut deck2) {
-            if card1 > card2 {
-                wins_hand(&mut deck1, card1, card2);
-            } else {
-                wins_hand(&mut deck2, card1, card2);
-            }
-        } else {
-            break;
-        }
-    }
+    let winner = recursive_combat(1, &mut deck1, &mut deck2);
 
-    if deck1.is_empty() {
-        calc_score(&deck2)
-    } else {
+    println!("Final 1 {:?}", deck1);
+    println!("Final 2 {:?}", deck2);
+    if winner == 1 {
         calc_score(&deck1)
+    } else {
+        calc_score(&deck2)
     }
 }
