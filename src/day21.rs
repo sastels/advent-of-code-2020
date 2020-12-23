95d203fd18c2523141732ee9a8cd42f94598c275
Steve Astels <sastels@gmail.com>
2020-12-22 21:54:31 -0500

    b

--- src/day21.rs
+++ src/day21.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
@@ -29,9 +30,7 @@ impl Food {
     }
 }
 
-pub fn solve_a(data: &[String]) -> usize {
-    let foods: Vec<Food> = data.iter().map(|line| Food::new(line)).collect();
-
+pub fn find_all_allergens(foods: &Vec<Food>) -> HashSet<String> {
     let all_allergens: HashSet<String> =
         foods
             .iter()
@@ -41,6 +40,10 @@ pub fn solve_a(data: &[String]) -> usize {
                 let union: HashSet<String> = union.iter().map(|s| s.to_string()).collect();
                 union
             });
+    all_allergens
+}
+
+pub fn find_all_ingredients(foods: &Vec<Food>) -> HashSet<String> {
     let all_ingredients: HashSet<String> =
         foods
             .iter()
@@ -50,7 +53,12 @@ pub fn solve_a(data: &[String]) -> usize {
                 let union: HashSet<String> = union.iter().map(|s| s.to_string()).collect();
                 union
             });
+    all_ingredients
+}
 
+pub fn find_possible_matches(foods: &Vec<Food>) -> HashMap<String, HashSet<String>> {
+    let all_allergens = find_all_allergens(&foods);
+    let all_ingredients = find_all_ingredients(&foods);
     let mut possible_ingredients: HashMap<String, HashSet<String>> = HashMap::new();
     for allergen in all_allergens {
         let ingredients = foods
@@ -65,14 +73,21 @@ pub fn solve_a(data: &[String]) -> usize {
             });
         possible_ingredients.insert(allergen.clone(), ingredients);
     }
+    possible_ingredients
+}
 
-    let maybe_bad = possible_ingredients
+pub fn solve_a(data: &[String]) -> usize {
+    let foods: Vec<Food> = data.iter().map(|line| Food::new(line)).collect();
+
+    let maybe_bad = find_possible_matches(&foods)
         .values()
         .fold(HashSet::new(), |acc, x| {
             let union: HashSet<_> = acc.union(&x).collect();
             let union: HashSet<String> = union.iter().map(|s| s.to_string()).collect();
             union
         });
+
+    let all_ingredients = find_all_ingredients(&foods);
 
     let diff: HashSet<_> = all_ingredients.difference(&maybe_bad).collect();
     let good_ingredients: HashSet<_> = diff.iter().map(|s| s.to_string()).collect();
@@ -85,9 +100,46 @@ pub fn solve_a(data: &[String]) -> usize {
                 .collect::<HashSet<_>>()
                 .len()
         })
-        .fold(0, |acc, x| acc + x)
+        .sum()
 }
 
-pub fn solve_b(_data: &[String]) -> usize {
-    unimplemented!()
+pub fn single_ingredient(list: &HashSet<String>) -> Option<String> {
+    let mut list: Vec<String> = list.iter().map(|s| s.to_string()).collect();
+
+    if list.len() == 1 {
+        list.drain(..).next()
+    } else {
+        None
+    }
+}
+
+pub fn solve_b(data: &[String]) -> String {
+    let foods: Vec<Food> = data.iter().map(|line| Food::new(line)).collect();
+    let mut matches = find_possible_matches(&foods);
+    let all_allergens = find_all_allergens(&foods);
+
+    let mut pairs: Vec<(String, String)> = vec![];
+    loop {
+        let mut changed = false;
+
+        for allergen in &all_allergens {
+            let matching_ingredient = single_ingredient(&matches.get(allergen).unwrap());
+            if matching_ingredient.is_some() {
+                let ingredient = matching_ingredient.unwrap();
+                pairs.push((allergen.clone(), ingredient.clone()));
+                for a in &all_allergens {
+                    if matches.get_mut(a).unwrap().contains(&ingredient) {
+                        changed = true;
+                    }
+                    matches.get_mut(a).unwrap().remove(&ingredient);
+                }
+            }
+        }
+        if !changed {
+            break;
+        }
+    }
+
+    pairs.sort();
+    pairs.iter().map(|(_, i)| i).join(",")
 }
--- tests/day21.rs
+++ tests/day21.rs
@@ -28,8 +28,7 @@ fn a() {
 }
 
 #[test]
-#[ignore]
 fn b() {
     let data = read_lines("./data/day21_test.txt");
-    assert_eq!(solve_b(&data), 0);
+    assert_eq!(solve_b(&data), "mxmxvkd,sqjhc,fvjkl");
 }
