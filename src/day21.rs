a8d2db9f7d4e2e669d52b9457b2f80eab0ca89d6
Steve Astels <sastels@gmail.com>
2020-12-22 21:03:05 -0500

    a

--- src/day21.rs
+++ src/day21.rs
@@ -1,5 +1,91 @@
-pub fn solve_a(_data: &[String]) -> usize {
-    unimplemented!()
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+pub struct Food {
+    pub ingredients: HashSet<String>,
+    pub allergens: HashSet<String>,
+}
+
+// trh fvjkl sbzzf mxmxvkd (contains dairy)
+
+impl Food {
+    pub fn new(line: &str) -> Self {
+        let line = line.replace(")", "").replace(",", "");
+        let mut parts = line.split(" (contains ");
+        Food {
+            ingredients: parts
+                .next()
+                .unwrap()
+                .split(" ")
+                .map(|s| s.to_string())
+                .collect(),
+            allergens: parts
+                .next()
+                .unwrap()
+                .split(" ")
+                .map(|s| s.to_string())
+                .collect(),
+        }
+    }
+}
+
+pub fn solve_a(data: &[String]) -> usize {
+    let foods: Vec<Food> = data.iter().map(|line| Food::new(line)).collect();
+
+    let all_allergens: HashSet<String> =
+        foods
+            .iter()
+            .map(|food| &food.allergens)
+            .fold(HashSet::new(), |acc, x| {
+                let union: HashSet<_> = acc.union(&x).collect();
+                let union: HashSet<String> = union.iter().map(|s| s.to_string()).collect();
+                union
+            });
+    let all_ingredients: HashSet<String> =
+        foods
+            .iter()
+            .map(|food| &food.ingredients)
+            .fold(HashSet::new(), |acc, x| {
+                let union: HashSet<_> = acc.union(&x).collect();
+                let union: HashSet<String> = union.iter().map(|s| s.to_string()).collect();
+                union
+            });
+
+    let mut possible_ingredients: HashMap<String, HashSet<String>> = HashMap::new();
+    for allergen in all_allergens {
+        let ingredients = foods
+            .iter()
+            .filter(|food| food.allergens.contains(&allergen))
+            .map(|food| &food.ingredients)
+            .fold(all_ingredients.clone(), |acc, x| {
+                let intersection: HashSet<_> = acc.intersection(&x).collect();
+                let intersection: HashSet<String> =
+                    intersection.iter().map(|s| s.to_string()).collect();
+                intersection
+            });
+        possible_ingredients.insert(allergen.clone(), ingredients);
+    }
+
+    let maybe_bad = possible_ingredients
+        .values()
+        .fold(HashSet::new(), |acc, x| {
+            let union: HashSet<_> = acc.union(&x).collect();
+            let union: HashSet<String> = union.iter().map(|s| s.to_string()).collect();
+            union
+        });
+
+    let diff: HashSet<_> = all_ingredients.difference(&maybe_bad).collect();
+    let good_ingredients: HashSet<_> = diff.iter().map(|s| s.to_string()).collect();
+
+    foods
+        .iter()
+        .map(|food| {
+            food.ingredients
+                .intersection(&good_ingredients)
+                .collect::<HashSet<_>>()
+                .len()
+        })
+        .fold(0, |acc, x| acc + x)
 }
 
 pub fn solve_b(_data: &[String]) -> usize {
--- tests/day21.rs
+++ tests/day21.rs
@@ -1,11 +1,30 @@
-use advent_2020::day21::{solve_a, solve_b};
+use advent_2020::day21::{solve_a, solve_b, Food};
 use advent_2020::utils::read_lines;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 #[test]
-#[ignore]
+fn test_food() {
+    let food = Food::new("mxmxvkd kfcds sqjhc nhms (contains dairy, fish)");
+    assert_eq!(
+        food.ingredients,
+        HashSet::from_iter(vec![
+            "mxmxvkd".to_string(),
+            "kfcds".to_string(),
+            "sqjhc".to_string(),
+            "nhms".to_string()
+        ])
+    );
+    assert_eq!(
+        food.allergens,
+        HashSet::from_iter(vec!["dairy".to_string(), "fish".to_string()])
+    );
+}
+
+#[test]
 fn a() {
     let data = read_lines("./data/day21_test.txt");
-    assert_eq!(solve_a(&data), 0);
+    assert_eq!(solve_a(&data), 5);
 }
 
 #[test]
